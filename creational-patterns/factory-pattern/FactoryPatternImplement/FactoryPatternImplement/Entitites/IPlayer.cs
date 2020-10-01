e838f1a0e3c4acbc43fb506cae10dc1d08a46805
redottergames <redottergames@gmail.com>
2020-10-01 13:02:14 -0300

    implementation of pattern factorymethod

---
+++ creational-patterns/factory-pattern/FactoryPatternImplement/FactoryPatternImplement/Entitites/IPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPatternImplement.Entitites
+{
+    public interface IPlayer
+    {
+        int Hp { get; }
+        int Mp { get; }
+        int For { get; }
+        int Int { get; }
+        int Sab { get; }
+        int Res { get; }
+
+        int Atack();
+        int Defence();
+    }
+}
---
+++ creational-patterns/factory-pattern/FactoryPatternImplement/FactoryPatternImplement/Entitites/WarriorPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPatternImplement.Entitites
+{
+    public class WarriorPlayer : IPlayer
+    {
+        public int Hp { get => 100; }
+        public int Mp { get => 50; }
+
+        public int For { get => 40; }
+        public int Int { get => 5; }
+        public int Res { get => 50; }
+        public int Sab { get => 40; }
+
+        public int Atack() => this.For * 2;
+        public int Defence() => this.Res * 3;
+    }
+}
